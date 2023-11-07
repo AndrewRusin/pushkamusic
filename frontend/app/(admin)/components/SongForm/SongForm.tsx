3443e0c9e2d97a1fc97716895de7c0ec504313d8
AndrewRusin <lombardx86@gmail.com>
2023-11-07 13:55:26 +0200

    song-edit title3

--- frontend/app/(admin)/components/SongForm/SongForm.tsx
+++ frontend/app/(admin)/components/SongForm/SongForm.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import { Button, Input } from '@/components';
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { ISongForm, ISongCategoriesResponse } from '@/interfaces/song.interface';
@@ -9,7 +9,6 @@ import { redirect } from 'next/navigation';
 import { createSongItems, findSongItem, patchSongItems } from '@/api/song';
 import { SongFormProps } from './SongForm.props';
 import { allFilesName, uploadFile } from '@/api/file';
-import { IUploadFile } from '@/interfaces/uploadFile.interface';
 import { getFilterItems, getFilterItemsModify } from '@/api/filter';
 import { IFilterCategory, IModernFilter } from '@/interfaces/CreateFilterForm.interface';
 import { FilterCheckbox } from '../FilterCheckbox/FilterCheckbox';
@@ -23,55 +22,48 @@ export function SongForm({ idItem = '', ...props }: SongFormProps): JSX.Element 
   const [file, setFile] = useState<File | null>(null);
   const [textAreaValue, setTextAreaValue] = useState<string>('');
   const [error, setError] = useState<string>();
-  const [isLoading, setIsLoading] = useState(false); // Добавили состояние для индикатора загрузки.
-  const [selectFileName, setSelectFileName] = useState<Options[] >([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectFileName, setSelectFileName] = useState<Options[]>([]);
 
   useEffect(() => {
-    if (idItem!=='') {
-      (async () => {
-      
-           
-        
-       
-      
-        const values = await findSongItem(idItem);
-        setValueForm(values);
-        setTextAreaValue(values.songsText)
-      })();
-    }
-    
-
-    return () => {
-      // This now gets called when the component unmounts.
-    };
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      const checkboxesObj = await getFilterItemsModify();
-      setCheckboxes(checkboxesObj);
-    })();
-    (async () => {
-      (async () => {
-      const res = await allFilesName()
-      console.log(res)
-        setSelectFileName(res)
-      
-      })();
-    })();
-    return () => {
-      // This now gets called when the component unmounts.
-    };
-  }, []);
-
-  function removeEmptyFields(data: any) {
-    Object.keys(data).forEach((key) => {
-      if (!data[key]) {
-        delete data[key];
+    // Fetch song item details
+    async function fetchSongItem() {
+      if (idItem !== '') {
+        try {
+          const values = await findSongItem(idItem);
+          setValueForm(values);
+          setTextAreaValue(values.songsText);
+        } catch (error) {
+          console.error('Error fetching song item:', error);
+        }
       }
-    });
-  }
-
+    }
+  
+    // Fetch checkboxes
+    async function fetchCheckboxes() {
+      try {
+        const checkboxesObj = await getFilterItemsModify();
+        setCheckboxes(checkboxesObj);
+      } catch (error) {
+        console.error('Error fetching checkboxes:', error);
+      }
+    }
+  
+    // Fetch file names
+    async function fetchFileNames() {
+      try {
+        const res = await allFilesName();
+        setSelectFileName(res);
+      } catch (error) {
+        console.error('Error fetching file names:', error);
+      }
+    }
+  
+    // Execute the async functions
+    fetchSongItem();
+    fetchCheckboxes();
+    fetchFileNames();
+  }, [idItem]);
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const selectedFile = event.target.files[0];
@@ -83,48 +75,44 @@ export function SongForm({ idItem = '', ...props }: SongFormProps): JSX.Element 
   const onSubmit: SubmitHandler<ISongForm> = async (data) => {
     removeEmptyFields(data);
     const totalData = { ...data, isHidden: isChecked, songsText: textAreaValue };
-    console.log({track_link:data.track_link })
-      if (valueForm) {
-        totalData.title = data.title || valueForm.title;
-        totalData.track_link = data.track_link  || valueForm.track_link;
-      }else {
-        if (!file) {
-          alert('Пожалуйста выберите файл');
-        }else{
-          try {
-            setIsLoading(true); 
-            const formData = new FormData();
-            const encodeFile = encodeURI(file.name);
-            formData.append('files', file, encodeFile);
-            const resFile = await uploadFile(formData);
-            const resultFile = [...resFile][0];
-            totalData.title = resultFile.name,
-            totalData.track_link = resultFile.url 
-          } catch (error) {
-            alert(error)
-          } finally {
-            setIsLoading(false);
-          }
+    if (valueForm) {
+      totalData.title = data.title || valueForm.title;
+      totalData.track_link = data.track_link || valueForm.track_link;
+    } else {
+      if (!file) {
+        alert('Пожалуйста выберите файл');
+        return;
+      } else {
+        try {
+          setIsLoading(true);
+          const formData = new FormData();
+          const encodeFile = encodeURI(file.name);
+          formData.append('files', file, encodeFile);
+          const resFile = await uploadFile(formData);
+          const resultFile = [...resFile][0];
+          totalData.title = resultFile.name,
+          totalData.track_link = resultFile.url;
+        } catch (error) {
+          alert(error);
+        } finally {
+          setIsLoading(false);
         }
       }
-    
-    if (textAreaValue==='') {
+    }
+    if (textAreaValue === '') {
       alert('Пожалуйста заполните текст песни');
       return;
     }
-    try { 
-      const resData = idItem!=='' ? await patchSongItems(totalData, idItem) : await createSongItems(totalData);
-
+    try {
+      const resData = idItem !== '' ? await patchSongItems(totalData, idItem) : await createSongItems(totalData);
       if (resData._id) {
         setRedirectTo(true);
       } else {
         setRedirectTo(false);
       }
     } catch (e) {
-      if (e instanceof Error) {
-        setError(e.message);
-      }
-    } 
+      console.log(e)
+    }
   };
 
   if (redirectTo) {
@@ -133,55 +121,73 @@ export function SongForm({ idItem = '', ...props }: SongFormProps): JSX.Element 
 
   return (
     <div {...props}>
-    
       <form onSubmit={handleSubmit(onSubmit)}>
-      {valueForm && selectFileName && <div className='filenames'><h1>{valueForm.title}, {valueForm.track_link}</h1>
-      <Input {...register('title')} placeholder='Имя' type='text' defaultValue={valueForm.title} />
-     <Controller
-     control={control}
-     name="track_link"
-     render={({ field: { onChange,  value, ref} }) => (
-     
-       <Select
-           className={styles.select}
-           value={selectFileName.find(option => option.value === value)}
-            onChange={(option) => onChange(option ? option.value : '')}
-           placeholder="Выберите параметр..."
-           options={selectFileName as any}
-           isClearable={true}
-           backspaceRemovesValue={true}
-           ref={ref}
-         
-       />
-     )}
-   /></div>
-
-}
-
-        {!valueForm && <div><input type="file" name="songFile" id="songFile" onChange={handleFileChange} /> {isLoading && <div className={styles.loadingIndicator}> Загрузка файла...</div>}</div>}
-       <div className={styles.textarea_wrapper}>
-        {textAreaValue.trim() === '' && <span className={styles.errorText}>Это поле обязательное поле</span>}
-        <div className={styles.textarea} contentEditable="true" onBlur={(e) => setTextAreaValue(e.currentTarget.innerHTML)} suppressContentEditableWarning={true}>
+        {valueForm && selectFileName && (
+          <div className='filenames'>
+            <h1>{valueForm.title}, {valueForm.track_link}</h1>
+            <Input {...register('title')} placeholder='Имя' type='text' defaultValue={valueForm.title} />
+            <Controller
+              control={control}
+              name="track_link"
+              render={({ field: { onChange, value, ref } }) => (
+                <Select
+                  className={styles.select}
+                  value={selectFileName.find(option => option.value === value)}
+                  onChange={(option) => onChange(option ? option.value : '')}
+                  placeholder="Выберите параметр..."
+                  options={selectFileName}
+                  isClearable={true}
+                  backspaceRemovesValue={true}
+                  ref={ref}
+                />
+              )}
+            />
+          </div>
+        )}
+        {!valueForm && (
+          <div>
+            <input type="file" name="songFile" id="songFile" onChange={handleFileChange} />
+            {isLoading && <div className={styles.loadingIndicator}>Загрузка файла...</div>}
+          </div>
+        )}
+        <div className={styles.textarea_wrapper}>
+          {textAreaValue.trim() === '' && <span className={styles.errorText}>Это поле обязательное поле</span>}
+          <div className={styles.textarea} contentEditable="true" onBlur={(e) => setTextAreaValue(e.currentTarget.innerHTML)} suppressContentEditableWarning={true}>
+            {valueForm ? (
+              <div dangerouslySetInnerHTML={{ __html: valueForm.songsText }} />
+            ) : null}
+          </div>
+        </div>
+        
         {valueForm ? (
-            <div dangerouslySetInnerHTML={{ __html: valueForm.songsText }} />
-          ) : null}
-        </div>
-        </div>
-        {valueForm ?<FilterCheckbox {...register('params')} filterItems={checkboxes} filterChecked={valueForm.params}/> :<FilterCheckbox {...register('params')} filterItems={checkboxes} filterChecked={[]}/>}
+          <div>
+          <h1>Есть</h1>
+          <FilterCheckbox {...register('params')} filterItems={checkboxes} filterChecked={valueForm.params}/>
+          </div>
+        ) : (
+          <FilterCheckbox {...register('params')} filterItems={checkboxes} filterChecked={[]}/>
+        )}
         <label>
-        <input
+          <input
             type="checkbox"
             name="hidden"
-            id=""
             checked={isChecked !== undefined ? isChecked : valueForm?.isHidden || false}
             onChange={() => setIsChecked((prev) => !prev)}
           />
           <span>скрыть песню</span>
         </label>
-        <Button appearance="primary" className={styles.button} type="submit" >
+        <Button appearance="primary" className={styles.button} type="submit">
           {idItem ? 'Изменить' : 'Создать'}
         </Button>
       </form>
     </div>
   );
+}
+
+function removeEmptyFields(data: any) {
+  Object.keys(data).forEach((key) => {
+    if (!data[key]) {
+      delete data[key];
+    }
+  });
 }
