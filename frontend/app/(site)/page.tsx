266a8142bf2509328d06d10b0ae426e70a539bfd
AndrewRusin <lombardx86@gmail.com>
2023-11-10 12:37:36 +0200

    select main changesv2

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -1,18 +1,12 @@
 
 import styles from './page.module.css'
-import { Button, Input } from '@/components'
-import Logo from '@/public/next.svg'
+import { Button } from '@/components'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className="container">
-      <Logo />
-      <Button appearance='primary' className='button'>Кнопка</Button>
-      <Button appearance='ghost'>Кнопка</Button>
-      <Button appearance='alert'>Кнопка</Button>
-      <Logo />
-      <span>sdsd44p</span>
+        <h1>Страница находится в разработке</h1>
       
       </div>
       
