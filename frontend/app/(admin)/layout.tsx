d688684cca3d402d9ebb807babd54d22aa5bcad3
AndrewRusin <lombardx86@gmail.com>
2023-11-16 16:31:24 +0200

    alpha ver0.2

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -1,5 +1,43 @@
 import { Sidebar } from "./components"
 import styles from "./layout.module.css"
+import '../styles/global.css';
+import localFont from "next/font/local";
+
+const sfProDisplay = localFont({
+  src: [
+    {
+      path: '../../public/fonts/SFProDisplay-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SFProDisplay-RegularItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/SFProDisplay-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SFProDisplay-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/SFProDisplay-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SFProDisplay-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+
+  ],
+})
 
 export const metadata = {
   title: 'Next.js',
@@ -13,7 +51,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body>
+      <body className={sfProDisplay.className}>
         <div className={styles.container}>
         <Sidebar />
         <div className={styles.content}>
